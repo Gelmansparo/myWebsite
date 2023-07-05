95e9553a13d38985c40236b3687c3b0137d78f4a
yaoyuting <2576728098@qq.com>
2023-07-05 11:52:59 +0800

    修改hash路由模式为history模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home/index.vue'
 import BlogList from '../views/BlogList/index.vue'
@@ -27,7 +27,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
